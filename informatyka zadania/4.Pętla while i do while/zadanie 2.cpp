733654f0ac99d644f37c08838e82f8fd1055ce28
Rakol353 <stefanborkowski351@gmail.com>
2025-03-11 16:08:38 +0100

    Update zadanie 2.cpp

--- informatyka zadania/4.Pętla while i do while/zadanie 2.cpp
+++ informatyka zadania/4.Pętla while i do while/zadanie 2.cpp
@@ -2,23 +2,19 @@
 using namespace std;
 
 int main() {
-    int N, suma = 0, i = 1;
+    int N;
+    int suma = 0;
+    int i = 1;
 
-    cout << "Podaj liczbe N: ";
+    cout << "Podaj liczbę N: ";
     cin >> N;
-
-    if (N <= 0) {
-        cout << "N musi byc liczba dodatnia!" << endl;
-        return 1;
-    }
 
     while (i <= N) {
         suma += i;
-        i++;
+        i++; 
     }
 
     cout << "Suma liczb naturalnych do " << N << ": " << suma << endl;
 
     return 0;
 }
-
