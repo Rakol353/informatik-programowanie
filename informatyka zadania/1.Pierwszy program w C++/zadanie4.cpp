b470ccd25dbd6277f318a028cba17e066b278340
Rakol353 <stefanborkowski351@gmail.com>
2025-03-11 15:52:59 +0100

    Update zadanie4.cpp

--- informatyka zadania/1.Pierwszy program w C++/zadanie4.cpp
+++ informatyka zadania/1.Pierwszy program w C++/zadanie4.cpp
@@ -7,14 +7,11 @@ int main() {
     string imie;
     int wiek;
 
-
     cout << "Podaj swoje imie: ";
     cin >> imie;
 
-
     cout << "Podaj swoj wiek: ";
     cin >> wiek;
-
 
     cout << "Witaj, " << imie << "! Masz " << wiek << " lat." << endl;
 
